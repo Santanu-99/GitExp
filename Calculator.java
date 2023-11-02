2a248f882e1b03863ac466076a8587315ebd4b5c
Santanu <bitup62@gmail.com>
2023-11-02 11:18:06 +0530

    Subtraction feature added

--- Calculator.java
+++ Calculator.java
@@ -1,3 +1,33 @@
+import java.util.*;
+
 public class Calculator{
-    
+    public static void main(String args[])throws IOException{
+        Scanner sc = new Scanner(System.in);
+        
+        char operator;
+        Double number1, number2, result;
+
+        System.out.println("Choose an operator: +, -, *, or /");
+        operator = sc.next().charAt(0);
+
+        // ask users to enter numbers
+        System.out.println("Enter first number");
+        number1 = sc.nextDouble();
+
+        System.out.println("Enter second number");
+        number2 = sc.nextDouble();
+
+        switch (operator) {
+            // performs addition between numbers
+            case '-':
+                Subtraction sub = new Subtraction();
+                result = sub.minus(number1 , number2);
+                System.out.println(number1 + " + " + number2 + " = " + result);
+                break;
+
+            default:
+                System.out.println("Invalid operator!");
+                break;
+        }
+    }
 }
